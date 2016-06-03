bdea5014cbaa5da532881ea040a7c183ca512add
Tim <tgaudette88@gmail.com>
2016-06-02 20:09:44 -0400

    Init commit, basic watcher to save tweets as issues

---
+++ README.md
@@ -0,0 +1,14 @@
+# GitHub Twissues
+Watch a hastag to record tweets as issues
+
+Replies to poster with link to issue on GitHub
+
+## Environment Variables
+- CONSUMERKEY - Twitter Consumer Key
+- CONSUMERSECRET - Twitter Consumer Secret
+- ACCESSTOKEN - Twitter Access Token
+- ACCESSTOKENSECRET - Twitter Access Token Secret
+- TRACKERTAG - hashtag to watch (without `#` prefix)
+- GITHUBTOKEN - GitHub access token
+- GITHUBREPO - Name of repository to post issues to
+- GITHUBUSER - Name of user repository is under
---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Twit = require('twit');
+var GitHubApi = require('github');
+
+// load twitter config
+var T = new Twit({
+  consumer_key:         process.env.CONSUMERKEY,
+  consumer_secret:      process.env.CONSUMERSECRET,
+  access_token:         process.env.ACCESSTOKEN,
+  access_token_secret:  process.env.ACCESSTOKENSECRET
+});
+
+// load github config
+var github = new GitHubApi({
+  protocol: 'https',
+  host: 'api.github.com'
+});
+
+// authenticate to github
+github.authenticate({
+  type: 'oauth',
+  token: process.env.GITHUBTOKEN
+});
+
+// begin watching for issue hashtag
+var stream = T.stream('statuses/filter', { track: '#' + process.env.TRACKERTAG });
+
+stream.on('tweet', function (tweet) {
+  if (!tweet.retweeted_status) { // only create issues for original tweets
+    github.issues.create({
+      user: process.env.GITHUBUSER,
+      repo: process.env.GITHUBREPO,
+      title: 'Twissue from ' + tweet.user.name,
+      body: tweet.text
+    }, function(err, res) {
+      var issueUrl = res.html_url;
+      T.post('statuses/update', { status: '@'+ tweet.user.screen_name + ' issue filed ' + issueUrl }, function(err, data, response) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+
+});
